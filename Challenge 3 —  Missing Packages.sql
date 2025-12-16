644d2f74c932239f3e7baac3b0f4ec2607778e3f
Zander Wepener <ZanderWepener5@gmail.com>
2025-12-16 16:39:31 +0200

    Update Challenge 3 —  Missing Packages.sql

--- Challenge 3 —  Missing Packages.sql
+++ Challenge 3 —  Missing Packages.sql
@@ -1,8 +1,8 @@
-SELECT  
+SELECT S.Carrier,  
 SUM(CASE WHEN S.DeliveredDate IS NULL THEN 1 ELSE 0 END) AS DidNotArrive,
 SUM(CASE WHEN S.DeliveredDate IS NOT NULL THEN 1 ELSE 0 END) AS DidArrive
 FROM SqlMiniProject.app.Shipments S
-
+GROUP BY S.Carrier;
 
 SELECT S.DeliveredDate, S.Carrier, S.TrackingNo, 
     CASE WHEN S.DeliveredDate IS NULL THEN 'Did Not Arrive' WHEN S.DeliveredDate IS NOT NULL 
